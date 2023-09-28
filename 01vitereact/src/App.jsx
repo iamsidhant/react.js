b117e310cd02d9862506672ceb0c1c26953f3fef
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-09-28 13:13:14 +0530

    jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,12 @@
 import Laptop from "./laptop"
 
 function App() {
-
+  const username = "computer aur code"
 
   return (
     <>
       <Laptop/>
-      <h1>laptop aur react</h1>
+      <h1>laptop aur react {username}</h1>
       <p>test para</p>
     </>
   )
