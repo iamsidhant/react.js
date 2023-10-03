4fad65b4d70931ed7a859f5ecd8582e83eef39ca
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-10-03 21:41:38 +0530

    react router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-600 p-5'>React Router</h1>
+    </>
+  )
+}
+
+export default App
