e203a296f69cc72cda1df1b1e96b8f57e330f0c7
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-10-04 13:54:59 +0530

    react router

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-
-
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-600 p-5'>React Router</h1>
+      
     </>
   )
 }
 
 export default App
+
