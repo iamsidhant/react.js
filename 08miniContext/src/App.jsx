8ab94220be52986bf6736a46c18b4b6e39a37046
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-10-06 12:55:33 +0530

    context api project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
   
@@ -10,6 +10,8 @@ function App() {
   return (
     <UserContextProvider>
       <h1>React with Hitesh Sir</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
