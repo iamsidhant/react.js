0ea4d393dcfe2c93459595d033c5a1e964df5650
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-10-09 20:19:11 +0530

    context and local storage

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { TodoProvider } from './contexts'
 import './App.css'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
